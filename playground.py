3439b66ab8c1bbc598e6664f7b20ce448e993ed5
Jing Zhang <jzhangc@gmail.com>
2024-08-25 12:34:46 -0400

    routine updates

---
+++ playground.py
@@ -0,0 +1,14 @@
+"""
+This is the playground for pytorch on both the NVIDIA and Apple platforms
+
+"""
+
+
+if torch.cuda.is_available():
+    device = "cuda"  # NVIDIA GPU
+elif torch.backends.mps.is_available():
+    device = "mps"  # Apple GPU
+else:
+    device = "cpu"  # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
+
+print(f"Using device: {device}")
